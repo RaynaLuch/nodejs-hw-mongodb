58521af9287c63d6cf096bdde9262fddf4f23e27
RaynaLuch <137286884+RaynaLuch@users.noreply.github.com>
2025-06-18 21:38:00 +0300

    HW7

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { UPLOAD_DIR } from './constants/index.js';
 
 dotenv.config();
 
@@ -27,6 +29,9 @@ export const setupServer = () => {
 
   app.use(cookieParser());
   app.use(router);
+
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,5 +1,5 @@
 import Joi from 'joi';
-import { isValidObjectId } from 'mongoose';
+//import { isValidObjectId } from 'mongoose';
 
 export const contactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
